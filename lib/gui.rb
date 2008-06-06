5771c1d9cc48a7d83db97f2707b5b3de1382c126
Italo Valcy <italo@dcc.ufba.br>
2008-06-06 04:02:10 +0000

    Adicionando entradas do meni...

--- lib/gui.rb
+++ lib/gui.rb
@@ -1,45 +1,65 @@
 require 'gtk2'  
 require 'libglade2'  
+
+class Simulador
+  def initialize()
+    Gtk.init
+    @glade = GladeXML.new('layout.glade', nil, 'simulador')  
+    window = @glade['simulador_window']
+    set_events()
+    window.show
+  end
+
+  def show
+    Gtk.main
+  end
   
-class SignalHandler  
-  def method_missing(method, *args)  
-    puts "#{method}: #{args}"   
-  end  
-end  
-sigmap = SignalHandler.new  
-  
-Gtk.init  
-glade = GladeXML.new('layout.glade', nil, 'simulador')  
-window = glade['simulador_window']
-glade.signal_autoconnect_full { |source, target, signal, handler, data, after| puts "#{handler} enviou o sinal: #{signal}" }
+  def iniciar_simulacao
+    puts "Simulacao iniciada..."
+  end
 
-# -- Labels --
-rg_ax = glade['rg_ax']
-rg_ax.text = "1001"
-bus_end_p_mem = glade['bus_end_p_mem']
-bus_end_p_mem.text = "1001100110011001"
-# -- Fim Labels -- 
+  def set_events
+    @glade['simulador_window'].signal_connect("destroy") { Gtk.main_quit }  
+    @glade['btn_input_hd'].signal_connect( "clicked" ) { input_hd() }
+    @glade['btn_input_net'].signal_connect( "clicked" ) { input_net() }
+    @glade['btn_input_key'].signal_connect( "clicked" ) { input_key() }
+    @glade['btn_iniciar'].signal_connect("clicked") { iniciar_simulacao() }
+    
+    # Cria os dialogs
+    ajuda_sobre = @glade['ajuda_sobre']
+    editar_pref = @glade['editar_pref']
+    mem_config = @glade['mem_config']
+    
+    ajuda_sobre.signal_connect("activate") { @glade['sobre_dialog'].show }
+    editar_pref.signal_connect("activate") { @glade['pref_dialog'].show }
+    mem_config.signal_connect('activate') { @glade['abrir_mem_arq'].show }
+    # Destroi os dialogs
+    @glade['btn_fechar_pref'].signal_connect('clicked') { pref_dialog.close }
+    @glade['btn_cancel_mem_arq'].signal_connect('clicked') { mem_config.close }
+  end
 
-# ---- Eventos ----
+  def set_value_rg(reg,value)
+    @glade["rg_#{reg}"].text = value
+  end
 
-btn_hd = glade['btn_input_hd']
-btn_hd.signal_connect( "clicked" ) { puts "Evento gerando pelo HD..." }
+  def set_value_bus_mem(bus,value)
+    @glade["bus_#{bus}_p_mem"].text = value
+  end
 
-btn_net = glade['btn_input_net']
-btn_net.signal_connect( "clicked" ) { puts "Evento gerando pela Rede..." }
+  def set_value_bus_io(bus,value)
+    @glade["bus_#{bus}_p_io"].text = value
+  end
 
-btn_teclado = glade['btn_input_key']
-btn_teclado.signal_connect( "clicked" ) { puts "Evento gerando pelo Teclado..." }
+  def input_hd
+    puts "Evento gerado pelo HD..."
+  end
 
-btn_iniciar = glade['btn_iniciar']
-btn_iniciar.signal_connect("clicked") { puts "Simulacao iniciada" }
+  def input_net
+    puts "Evento gerando pela Rede..."
+  end
 
-# -- Fim eventos --
+  def input_key
+    puts "Evento gerando pelo Teclado..."
+  end
 
-
-window.signal_connect("destroy") { Gtk.main_quit }  
-glade.signal_autoconnect_full { |source, target, signal, handler, data| puts "#{handler} enviou o sinal: #{signal}" }
-#  source.signal_connect(signal) { sigmap.send(handler, data); puts "#{handler} #{data}" }
-  
-window.show  
-Gtk.main  
+end
