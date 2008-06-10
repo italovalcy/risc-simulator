a2d2095d3682a4a3a3b8e1f564d6a446a9758b5b
Italo Valcy <italo@dcc.ufba.br>
2008-06-10 05:17:06 +0000

    Implementando algumas coisas no layout. Melhorando implementacao da memoria. Implementando algumas configurações de preferencias. TODO: os combobox de editar->preferencias nao estao ficando com o valor padrão; A funcao de diminuir o tamanho da memoria não está correta.

--- lib/gui.rb
+++ lib/gui.rb
@@ -4,10 +4,14 @@ require 'libglade2'
 class Simulador
   def initialize()
     @made_clock = false
+    @tam_mem = 64
+    @tam_cache = 16
 
     Gtk.init
     @@glade = GladeXML.new('layout.glade', nil, 'simulador')  
     window = @@glade['simulador_window']
+    make_gridview(@@glade['gridview_cache'],@tam_cache)
+    make_gridview(@@glade['gridview_mem'],@tam_mem)
     set_events()
     window.show
   end
@@ -15,9 +19,43 @@ class Simulador
   def show
     Gtk.main
   end
-  
-  def iniciar_simulacao
-    puts "Simulacao iniciada..."
+ 
+  def make_gridview(view,tam)
+    list = Gtk::ListStore.new(String,String)
+
+    # Adiciona elementos a lista
+    for i in 0..tam - 1
+      iter = list.append
+      iter[0] = i.to_s
+      iter[1] = "0"
+    end
+
+    r1 = Gtk::CellRendererText.new
+    r2 = Gtk::CellRendererText.new
+    c1 = Gtk::TreeViewColumn.new("",r1, :text => 0)
+    c2 = Gtk::TreeViewColumn.new("Value",r2, :text => 1)
+    
+    view.append_column(c1)
+    view.append_column(c2)
+    view.selection.mode = Gtk::SELECTION_NONE
+    view.model = list
+  end
+
+  def resize_gridview(view,tam_new)
+    list = view.model
+    if (@tam_mem < tam_new )
+      for i in @tam_mem..tam_new - 1
+        iter = list.append
+        iter[0] = i.to_s
+        iter[1] = "0"
+      end
+    else
+      while (@tam_mem >= tam_new)
+        @tam_mem -= 1
+        list.remove(list.get_iter(@tam_mem.to_s))
+      end
+    end
+    @tam_mem = tam_new
   end
 
   def set_events
@@ -37,11 +75,20 @@ class Simulador
     @@glade['sobre_dialog'].signal_connect("delete_event") { @@glade['sobre_dialog'].hide }
     @@glade['sobre_dialog'].signal_connect("response") { |s,r| if (r==Gtk::Dialog::RESPONSE_CANCEL); s.hide; end }
     @@glade['pref_dialog'].signal_connect("delete_event") { @@glade['pref_dialog'].hide }
-    @@glade['btn_fechar_pref'].signal_connect("clicked") { @@glade['pref_dialog'].hide }
+    @@glade['btn_fechar_pref'].signal_connect("clicked") { event_fechar_pref() }
     @@glade['abrir_mem_arq'].signal_connect("delete_event") { @@glade['abrir_mem_arq'].hide }
     @@glade['btn_cancel_mem_arq'].signal_connect("clicked") { @@glade['abrir_mem_arq'].hide }
   end
 
+  def event_fechar_pref
+    @@glade['pref_dialog'].hide
+    if (@@glade['mem_size'].text.to_i != @tam_mem)
+      resize_gridview(@@glade['gridview_mem'],@@glade['mem_size'].text.to_i)
+    end
+    if (@@glade['cache_size'].active_text.to_i != @tam_mem)
+      resize_gridview(@@glade['gridview_cache'], @@glade['cache_size'].active_text.to_i)
+    end
+  end
 
   def Simulador.set_value_rg(reg,value)
     @@glade["rg_#{reg}"].text = value
@@ -92,6 +139,9 @@ class Simulador
     return @@glade['clock_type'].get_active_text == "Automatico"
   end
 
+  def iniciar_simulacao
+    puts "Simulacao iniciada..."
+  end
 
   def input_hd
     puts "Evento gerado pelo HD..."
